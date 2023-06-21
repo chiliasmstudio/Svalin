389a2bbe3e856880c4bbca5f3cbddb304c91730c
paul <paul002706@gmail.com>
2023-06-22 03:24:56 +0800

    Brrrt

---
+++ src/main/java/com.chiliasmstudio.Svalin/Svalin.java
@@ -0,0 +1,10 @@
+package com.chiliasmstudio.Svalin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Svalin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello world!");
+    }
+}
